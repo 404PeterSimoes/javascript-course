8f14ced51e35ea37f8336c36009cf8eddc70103c
Pedro Simoes <176055646+404PeterSimoes@users.noreply.github.com>
2025-07-24 17:06:01 +0100

    todas as funções criadas

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,13 +4,48 @@ let currentResult = defaultResult;
 function getUserNumberInput() {
     return parseInt(userInput.value);
 }
+/*
+function calcAndWriteOutput(operator, resultBeforeCalc, calcInput) {
+    const
+}*/
 
 function add() {
     const enteredNumber = getUserNumberInput();
 
     const calcDescription = `${currentResult} + ${enteredNumber}`;
     currentResult += enteredNumber;
+
+    outputResult(currentResult, calcDescription);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+
+    const calcDescription = `${currentResult} - ${enteredNumber}`;
+    currentResult -= enteredNumber;
+
+    outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+
+    const calcDescription = `${currentResult} * ${enteredNumber}`;
+    currentResult *= enteredNumber;
+
+    outputResult(currentResult, calcDescription);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+
+    const calcDescription = `${currentResult} / ${enteredNumber}`;
+    currentResult /= enteredNumber;
+
     outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
